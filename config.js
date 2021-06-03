1d0701f22da735a6f6fb643c80a3aaf0b5b11d78
MinhA1802 <85271922+MinhA1802@users.noreply.github.com>
2021-06-03 12:31:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 const CONFIG = {
     introTitle: 'QAnhhhhh!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao Anh cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
+    introDesc: `Trái đất vốn lạ thường. 
+    Mà sao Anh cứ đi nhầm đường.
+    Lạc vào tim em lẻ loi.
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng Anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    btnIntro: 'hehe',
+    title: 'Phải chăng Anh đã yêu ngay từ cái nhìn đầu tiên.',
     desc: 'Phải chăng Anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích em <33',
+    btnYes: 'Vẫn cứ là yêu em <33',
     btnNo: 'Không, em à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao Anh lại yêu em <3',
-    btnReply: 'Gửi cho em <3',
+    btnReply: 'send to me <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em biết mà 🥰. Yêu QAnh 😘😘',}
+    mess: 'Em biết mà. Yêu QAnh',}
